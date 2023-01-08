88a3c16e271aa7a963af71de4d561b5533b75528
CD <1595292846@qq.com>
2023-01-08 18:43:52 +0800

    create project

---
+++ src/Abc.java
@@ -0,0 +1,12 @@
+/**
+ * @author CD
+ * @since 2023/1/8 18:42
+ * package: PACKAGE_NAME
+ * class: Abc
+ */
+public class Abc {
+    public static void main(String[] args) {
+        System.out.println("anc");
+        System.out.println("啊沙发上");
+    }
+}
