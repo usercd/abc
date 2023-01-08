a45505b56b4ca93cd8666cfd0b91428842cb8b3c
CD <1595292846@qq.com>
2023-01-08 18:33:12 +0800

    Test

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author CD
+ * @since 2023/1/8 18:32
+ * package: PACKAGE_NAME
+ * class: Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好世界");
+        System.out.println("Hello World");
+    }
+}
